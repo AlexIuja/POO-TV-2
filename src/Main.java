9e6398b457fd1f9da6d34973fa5b08817b9aa2a4
stillscript <george.iuja@stud.acs.upb.ro>
2022-12-31 20:49:43 +0200

    Started homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import classes.fileio.Input;
+import classes.packet.Site;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
+        Input inputData = objectMapper.readValue(new File("D:\\CA\\POO\\POO TV 2\\checker\\resources\\in\\basic_1.json"), Input.class);
+        Site site = new Site(inputData.getUsers(), inputData.getMovies(), inputData.getActions());
+        ArrayNode output = objectMapper.createArrayNode();
+        //site.exec(site.getActionsIn(), objectWriter, objectMapper, output, args);
+       // System.out.println(args[0] + " " + args[1]);
     }
 }
